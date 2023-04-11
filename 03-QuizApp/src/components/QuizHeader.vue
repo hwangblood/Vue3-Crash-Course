8dd529db14f32c387236832e57e36b9d0f31355d
hwangblood <hwangblood@gmail.com>
2023-04-11 22:35:00 +0800

    quiz: Render quiz's questions

---
+++ 03-QuizApp/src/components/QuizHeader.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+
+<template>
+  <div>
+    <header>
+      <h4>Question 1/3</h4>
+      <div class="bar">
+        <div class="completion"></div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+header {
+  margin-top: 20px;
+}
+header h4 {
+  font-size: 30px;
+}
+
+.bar {
+  width: 300px;
+  height: 50px;
+  border: 3px solid bisque;
+}
+.completion {
+  width: 0%;
+  height: 100%;
+  background-color: bisque;
+}
+</style>
