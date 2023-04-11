b71c0f068f8cc74e261da4bd22831581aa6dc51d
hwangblood <hwangblood@gmail.com>
2023-04-11 20:20:56 +0800

    quiz: Setup vue-router

---
+++ 03-QuizApp/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import QuizesView from "../views/QuizesView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "quizes",
+      component: QuizesView,
+    },
+    {
+      path: "/home",
+      redirect: "/",
+    },
+    {
+      path: "/quizes",
+      redirect: "/",
+    },
+  ],
+});
+
+export default router;
